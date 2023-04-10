7677d591fcc42a02320e0d30837be61885d163f5
Beggie98 <bjuraev2020@gmail.com>
2023-04-10 14:46:02 +0500

    added GitHub account

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,16 @@ public class GitCool {
     //B23 is learning git
     //B23 is a special number
     //B23 is making some sense now
+
+    //We have added out GitHub account under IntelliJ
+    //make sure to click apply and click on to save
+
+    //we were able to successfully share project under our github account
+    //Share Project on github happen only one time per project
+    //it combines the 3 actions below
+    //Create remote repository under your account
+    //Create the link between local repo to remote repo
+    //Push local commits to remote repo
+
+
 }
